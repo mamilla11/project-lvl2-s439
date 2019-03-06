eb6f60ea04d0eb1ca0c7f41cd0e44710d733a302
anashi <n_lazareva@mail.ru>
2019-03-06 16:54:32 +0300

    ini files diff added

--- src/parsers.js
+++ src/parsers.js
@@ -1,8 +1,10 @@
 import yaml from 'js-yaml';
+import ini from 'ini';
 
 const parsers = {
   '.json': JSON.parse,
   '.yaml': yaml.safeLoad,
+  '.ini': ini.parse,
 };
 
 export default (extension, content) => parsers[extension](content);
