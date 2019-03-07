02d3ed8a192eb6457eb20193bd9ba33898fcf740
anashi <n_lazareva@mail.ru>
2019-03-07 13:27:54 +0300

    tests for nested config files added

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,62 +1,63 @@
 import fs from 'fs';
 import genDiff from '../src';
 
+const filePaths = {
+  flatTest: {
+    JSONFilesTest: {
+      oldConfigFile: '__tests__/__fixtures__/beforeFlat.json',
+      newConfigFile: '__tests__/__fixtures__/afterFlat.json',
+      correctDiffFile: '__tests__/__fixtures__/resultFlat.txt',
+    },
+    YAMLFilesTest: {
+      oldConfigFile: '__tests__/__fixtures__/beforeFlat.yaml',
+      newConfigFile: '__tests__/__fixtures__/afterFlat.yaml',
+      correctDiffFile: '__tests__/__fixtures__/resultFlat.txt',
+    },
+    INIFilesTest: {
+      oldConfigFile: '__tests__/__fixtures__/beforeFlat.ini',
+      newConfigFile: '__tests__/__fixtures__/afterFlat.ini',
+      correctDiffFile: '__tests__/__fixtures__/resultFlat.txt',
+    },
+  },
+
+  nestedTest: {
+    JSONFilesTest: {
+      oldConfigFile: '__tests__/__fixtures__/beforeNested.json',
+      newConfigFile: '__tests__/__fixtures__/afterNested.json',
+      correctDiffFile: '__tests__/__fixtures__/resultNested.txt',
+    },
+    YAMLFilesTest: {
+      oldConfigFile: '__tests__/__fixtures__/beforeNested.yaml',
+      newConfigFile: '__tests__/__fixtures__/afterNested.yaml',
+      correctDiffFile: '__tests__/__fixtures__/resultNested.txt',
+    },
+    INIFilesTest: {
+      oldConfigFile: '__tests__/__fixtures__/beforeNested.ini',
+      newConfigFile: '__tests__/__fixtures__/afterNested.ini',
+      correctDiffFile: '__tests__/__fixtures__/resultNested.txt',
+    },
+  },
+};
+
+const executeTests = (testData) => {
+  it.each(testData)(
+    '.genDiff(%s, %s)',
+    (oldFilePath, newFilePath, correctFilePath) => {
+      const expected = fs.readFileSync(correctFilePath, 'utf8');
+      const received = genDiff(oldFilePath, newFilePath);
+      expect(received).toEqual(expected);
+    },
+  );
+};
+
+const prepareTestData = paths => Object.keys(paths).map(key => Object.values(paths[key]));
+
 describe('GenDiff Flat', () => {
-  const oldConfigFilePathJSON = '__tests__/__fixtures__/beforeFlat.json';
-  const newConfigFilePathJSON = '__tests__/__fixtures__/afterFlat.json';
-
-  const oldConfigFilePathYAML = '__tests__/__fixtures__/beforeFlat.yaml';
-  const newConfigFilePathYAML = '__tests__/__fixtures__/afterFlat.yaml';
-
-  const oldConfigFilePathINI = '__tests__/__fixtures__/beforeFlat.ini';
-  const newConfigFilePathINI = '__tests__/__fixtures__/afterFlat.ini';
-
-  const correctDiffFilePath = '__tests__/__fixtures__/resultFlat.txt';
-
-  const expected = fs.readFileSync(correctDiffFilePath, 'utf8');
-
-  it('json files', () => {
-    const received = genDiff(oldConfigFilePathJSON, newConfigFilePathJSON);
-    expect(received).toEqual(expected);
-  });
-
-  it('yaml files', () => {
-    const received = genDiff(oldConfigFilePathYAML, newConfigFilePathYAML);
-    expect(received).toEqual(expected);
-  });
-
-  it('ini files', () => {
-    const received = genDiff(oldConfigFilePathINI, newConfigFilePathINI);
-    expect(received).toEqual(expected);
-  });
+  const paths = filePaths.flatTest;
+  executeTests(prepareTestData(paths));
 });
 
 describe('GenDiff Nested', () => {
-  const oldConfigFilePathJSON = '__tests__/__fixtures__/beforeNested.json';
-  const newConfigFilePathJSON = '__tests__/__fixtures__/afterNested.json';
-
-  // const oldConfigFilePathYAML = '__tests__/__fixtures__/beforeNested.yaml';
-  // const newConfigFilePathYAML = '__tests__/__fixtures__/afterNested.yaml';
-
-  // const oldConfigFilePathINI = '__tests__/__fixtures__/beforeNested.ini';
-  // const newConfigFilePathINI = '__tests__/__fixtures__/afterNested.ini';
-
-  const correctDiffFilePath = '__tests__/__fixtures__/resultNested.txt';
-
-  const expected = fs.readFileSync(correctDiffFilePath, 'utf8');
-
-  it('json files', () => {
-    const received = genDiff(oldConfigFilePathJSON, newConfigFilePathJSON);
-    expect(received).toEqual(expected);
-  });
-
-  // it('yaml files', () => {
-  //   const received = genDiff(oldConfigFilePathYAML, newConfigFilePathYAML);
-  //   expect(received).toEqual(expected);
-  // });
-
-  // it('ini files', () => {
-  //   const received = genDiff(oldConfigFilePathINI, newConfigFilePathINI);
-  //   expect(received).toEqual(expected);
-  // });
+  const paths = filePaths.nestedTest;
+  executeTests(prepareTestData(paths));
 });
